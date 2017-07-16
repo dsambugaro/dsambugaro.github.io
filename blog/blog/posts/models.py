b23bb2a2caf477a2bdc08840db3e80ca121b09cd
dsambugaro <sambugaro.com@gmail.com>
2017-07-16 10:35:55 +0000

    removendo template

--- blog/blog/posts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-
-    titulo = models.CharField(max_length = 255, null = False)
-    autor = models.CharField(max_length = 255, null = False)
-    data = models.DateField(null = False)
-    conteudo = models.TextField(null = False)
--- blog/blog/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from posts import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^posts/(?P<post_id>\w+)$', views.post, name='post'),
-]
